8e875daad84f96ea4f92cd2cc26563b5de9d3a94
MKriukov <tbiwka@yandex.ru>
2020-03-20 13:49:58 +0200

    Tutorial1

---
+++ Assets/Tutorial1/SimpleFilter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SimpleFilter : MonoBehaviour
+{
+    [SerializeField]
+    private Shader _shader;
+
+    private Material _material;
+    private bool _useFilter = true;
+
+    private void Awake()
+    {
+        _material = new Material(_shader);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            _useFilter = !_useFilter;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (_useFilter)
+        {
+            Graphics.Blit(src, dst, _material);
+        }
+        else
+        {
+            Graphics.Blit(src, dst);
+        }
+    }
+}
