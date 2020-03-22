4d954813d31707830bff14e577c54df6ca649ae8
MKriukov <tbiwka@yandex.ru>
2020-03-22 13:53:59 +0200

    Record1

---
+++ Assets/Common/CameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 10f;
+    [SerializeField]
+    private float _rotationSpeed = 3.0f;
+
+    private Vector2 _rotation = Vector2.zero;
+    private Vector3 _moveVector = Vector3.zero;
+
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        _rotation += new Vector2(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+        transform.eulerAngles = _rotation * _rotationSpeed;
+
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        _moveVector = new Vector3(x, 0, z) * _moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = transform.TransformDirection(_moveVector);
+    }
+}
---
+++ Assets/Common/SimpleFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFilter : MonoBehaviour
+{
+    [SerializeField] private Shader _shader;
+
+    private Material _mat;
+
+    private bool _useFilter = true;
+
+    private void Awake()
+    {
+        _mat = new Material(_shader);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            _useFilter = !_useFilter;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (_useFilter)
+        {
+            Graphics.Blit(src, dst, _mat);
+        }
+        else
+        {
+            Graphics.Blit(src, dst);
+        }
+    }
+}
