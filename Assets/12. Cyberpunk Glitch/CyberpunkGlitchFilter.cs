6262339dd1ad7dfd5b683d777749c4aa26f1b48b
gaitavr <tbiwka@yandex.ru>
2020-12-29 00:12:39 +0200

    Cyberpunk glitch

---
+++ Assets/12. Cyberpunk Glitch/CyberpunkGlitchFilter.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class CyberpunkGlitchFilter : SimpleFilter
+{
+    [SerializeField, Range(0, 1f)]
+    private float _colorIntensity = 1f;
+    private float _flickerTimer;
+    private float _flickerTime = 0.5f;
+    
+    [SerializeField, Range(0, 1)]
+    private float _noiseIntensity = 1f;
+    [SerializeField]
+    private Texture2D _noiseTexture;
+    
+    [SerializeField, Range(0, 1)]
+    private float _flipIntensity = 1f;
+    private float _flipUpTimer;
+    private float _flipUpTime = 0.05f;
+    private float _flipDownTimer;
+    private float _flipDownTime = 0.05f;
+    
+    protected override void UseFilter(RenderTexture src, RenderTexture dst)
+    {
+        _flipUpTimer += Time.deltaTime * _flipIntensity;
+        if (_flipUpTimer > _flipUpTime)
+        {
+            if (Random.value < 0.1f * _flipIntensity)
+            {
+                _mat.SetFloat("_FlipUp", Random.value * _flipIntensity);
+            }
+            else
+            {
+                _mat.SetFloat("_FlipUp", 0);
+            }
+            _flipUpTimer = 0;
+            _flipUpTime = Random.value * 0.1f;
+        }
+
+        _flipDownTimer += Time.deltaTime * _flipIntensity;
+        if (_flipDownTimer > _flipDownTime)
+        {
+            if (Random.value < 0.1f * _flipIntensity)
+            {
+                _mat.SetFloat("_FlipDown", 1 - Random.value * _flipIntensity);
+            }
+            else
+            {
+                _mat.SetFloat("_FlipDown", 1);
+            }
+            _flipDownTimer = 0;
+            _flipDownTime = Random.value * 0.1f;
+        }
+        
+        if (_flipIntensity == 0)
+        {
+            _mat.SetFloat("_FlipUp", 0);
+            _mat.SetFloat("_FlipDown", 1);
+        }
+        
+        _mat.SetTexture("_NoiseTex", _noiseTexture);
+        if (Random.value < 0.05 * _noiseIntensity)
+        {
+            _mat.SetFloat("_DisplacementFactor", Random.value * _noiseIntensity);
+            _mat.SetFloat("_NoiseScale", 1 - Random.value - _noiseIntensity);
+        }
+        else
+        {
+            _mat.SetFloat("_DisplacementFactor", 0);
+        }
+        
+        _flickerTimer += Time.deltaTime * _colorIntensity;
+        if (_flickerTimer > _flickerTime)
+        {
+            _mat.SetVector("_ColorDirection", Random.insideUnitCircle);
+            _mat.SetFloat("_ColorRadius", Random.Range(-3f, 3f) * 
+                                          _colorIntensity);
+            _flickerTimer = 0;
+            _flickerTime = Random.value;
+        }
+
+        if (_colorIntensity == 0)
+        {
+            _mat.SetFloat("_ColorRadius", 0);
+        }
+        Graphics.Blit(src, dst, _mat);
+    }
+}
---
+++ Assets/12. Cyberpunk Glitch/CyberpunkGlitchFilter.shader
@@ -0,0 +1,75 @@
+﻿Shader "Hidden/CyberpunkGlitchFilter"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+            fixed2 _ColorDirection;
+            half _ColorRadius;
+            
+            sampler2D _NoiseTex;
+            half _DisplacementFactor;
+            half _NoiseScale;
+            
+            half _FlipUp;
+            half _FlipDown;
+            
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float2 uv = i.uv;
+         
+                uv.y -= (1 - (uv.y + _FlipUp)) * step(uv.y, _FlipUp) + 
+                    (1 - (uv.y - _FlipDown)) * step(_FlipDown, uv.y);
+                
+                fixed4 noise = tex2D(_NoiseTex, i.uv * _NoiseScale);
+                uv += (noise.rg - 0.5) * _DisplacementFactor;
+
+                half4 col = tex2D(_MainTex, uv);
+                half4 colR = tex2D(_MainTex, uv + _ColorDirection * _ColorRadius * 
+                    0.01);
+                half4 colG = tex2D(_MainTex, uv - _ColorDirection * _ColorRadius * 
+                    0.01);
+                float threshold = 0.5;
+                float lum = 0.5;
+                col.rgb += colR.rbg * step(_ColorRadius, -threshold) * lum;
+                col.rgb += colG.gbr * step(threshold, _ColorRadius) * lum;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
